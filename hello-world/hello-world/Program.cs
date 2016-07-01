3eadcce97a5425f6e7d6112bb3b8696627ac7897
dawoodhanna <dawoodhanna100@yahoo.com>
2016-07-01 10:49:30 -0700

    creating a new project from scratch

---
+++ hello-world/hello-world/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: dawood
+ * Date: 2016-07-01
+ * Time: 10:43 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace hello_world
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
