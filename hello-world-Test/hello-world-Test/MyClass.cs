bfcba8442c471448f164a46aae155eeb7f43787c
dawoodhanna <dawoodhanna100@yahoo.com>
2016-07-01 13:16:41 -0700

    creating unit test project

---
+++ hello-world-Test/hello-world-Test/MyClass.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: dawood
+ * Date: 2016-07-01
+ * Time: 1:04 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+
+
+namespace hello_world_Test
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public class MyClass
+	{
+		
+	}
+}
---
+++ hello-world-Test/hello-world-Test/hello-world-UnitTest.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: dawood
+ * Date: 2016-07-01
+ * Time: 1:08 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using NUnit.Framework;
+using hello_world;
+
+namespace hello_world_Test
+{
+	[TestFixture]
+	public class hello_world_UnitTest
+	{
+		[Test]
+		public void TestMethod()
+		{
+			// TODO: Add your test.
+			hello_world.Program.SetUp();
+			Assert.AreEqual(99,hello_world.Program.number);
+		}
+	}
+}
